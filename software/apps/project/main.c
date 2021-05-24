f956b8d1a13634064c982425704dd93ce7b68a2b
sgh185 <sgh185@sgh185s-MBP.cable.rcn.com>
2021-05-24 05:02:12 -0500

    driver: add main method

---
+++ software/apps/project/main.c
@@ -0,0 +1,43 @@
+/*
+ * main.c 
+ *
+ * Driver for heart monitor system
+ */ 
+
+#include "monitor.h"
+#include "simulator.h"
+#include "utils.h"
+
+/*
+ * Setup functions
+ */ 
+typedef void (*monitor_setup)(monitor *);
+
+monitor_setup monitor_setups[MONITOR_TYPES] = {
+    simulator_monitor_handler_setup,
+    NULL
+};
+
+
+/*
+ * Global timer for monitor
+ */ 
+APP_TIMER_DEF(monitor_timer);
+
+
+int main(void) 
+{
+    /*
+     * Setup board and monitor
+     */
+    printf("Board started!\n");
+    
+    the_monitor = 
+	bootstrap_monitor(
+	    monitor_setups[SETUP_NO],
+	    &monitor_timer
+	);
+
+    while (1) nrf_delay_ms(1000);
+}
+
