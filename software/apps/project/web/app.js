d363153f03c795f8fb8741ee32753d5259fd370a
sgh185 <sgh185@sgh185s-MBP.cable.rcn.com>
2021-05-26 22:47:55 -0500

    web: add parsing for detection status

--- software/apps/project/web/app.js
+++ software/apps/project/web/app.js
@@ -10,7 +10,7 @@
 /*
  * Connection to serial output 
  */
-const DEBUG = 0;
+const DEBUG = 1;
 
 const argv = require('minimist')(process.argv.slice(2));
 const path = argv["d"];
@@ -29,9 +29,19 @@ const device = new SerialPort(path, {
  * Parsing, Global state
  */ 
 const history_size = 16;
+
+const rate_status = {
+    "N" : "Normal",
+    "H" : "**Rate HIGH**",
+    "L" : "**Rate LOW**",
+    "R" : "**Rising Rapidly**",
+    "F" : "**Falling Rapidly**"
+} ;
+
 const monitor_state = {
     "mode" : "0",
-    "data" : new CBuffer(history_size)
+    "data" : new CBuffer(history_size),
+    "status" : "Normal"
 } ;
 
 const parser = device.pipe(new Readline({ delimiter: '\n' }));
@@ -56,13 +66,21 @@ parser.on('data', (data) => {
 	const new_mode = data_str.substring(5, 6);
 	monitor_state["mode"] = new_mode; 
     }
-    else if (prefix == "DATA")
+    else if (prefix == "BEAT")
     {
 	/*
 	 * Update history
 	 */ 
 	const new_mode = data_str.substring(5);
 	monitor_state["data"].push(Number(new_mode));
+    }
+    else if (prefix == "RATE")
+    {
+	/*
+	 * Update status based on rate evaluation
+	 */ 
+	const curr_status = rate_status[data_str.substring(5)];
+	monitor_state["status"] = curr_status;
     }
 
 
@@ -73,6 +91,7 @@ parser.on('data', (data) => {
     { 
 	console.log("monitor_state[mode]: " + monitor_state["mode"]);
 	console.log("monitor_state[data]: " + monitor_state["data"].toArray().toString());
+	console.log("monitor_state[status]: " + monitor_state["status"]);
     }
 
 });
